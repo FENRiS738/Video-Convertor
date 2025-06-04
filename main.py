7c524ea51d43e01c719d2f45964de0784b474de6
FENRiS738 <as420738@gmail.com>
2025-06-05 01:22:26 +0530

    Updated script to use whisper

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
+from pydantic import BaseModel
 import os
+import whisper
 from fastapi import UploadFile
 from moviepy import VideoFileClip
-import speech_recognition as sr
 
 app = FastAPI()
 
 UPLOAD_DIR = "uploads"
 CHUNK_SIZE = 1024 * 1024
 os.makedirs(UPLOAD_DIR, exist_ok=True)
+model = whisper.load_model("base", "cpu")
+
+class AudioFile(BaseModel):
+    filename: str
 
 @app.get("/", response_class=JSONResponse)
 async def root(request: Request):
@@ -24,30 +29,8 @@ def extract_audio_from_video(video_path, audio_path):
     audio.close()
     video.close()
 
-def transcribe_audio(audio_path):
-    recognizer = sr.Recognizer()
-
-    with sr.AudioFile(audio_path) as source:
-        duration = source.DURATION
-        step = 30
-        transcript = ""
-
-        for i in range(0, int(duration), step):
-            source_audio = recognizer.record(source, duration=step)
-            try:
-                chunk_text = recognizer.recognize_google(source_audio)
-                transcript += chunk_text + " "
-            except sr.UnknownValueError:
-                transcript += "[Unclear] "
-            except sr.RequestError as e:
-                transcript += f"[Error: {e}] "
-                break
-
-        return transcript.strip()
-
-        
-@app.post("/upload", response_class=JSONResponse)
-async def upload_video(file: UploadFile):
+@app.post("/video-to-audio", response_class=JSONResponse)
+async def convert_video_to_audio(file: UploadFile):
     try:
         video_path = os.path.join(UPLOAD_DIR, file.filename)
         audio_path = os.path.join(UPLOAD_DIR, f"{os.path.splitext(file.filename)[0]}.wav")
@@ -60,14 +43,35 @@ async def upload_video(file: UploadFile):
                 f.write(chunk)
         
         extract_audio_from_video(video_path, audio_path)
-        transcript = transcribe_audio(audio_path)
-        
         os.remove(video_path)
-        os.remove(audio_path)
         
         return {
-            "message": "Video uploaded and transcribed successfully",
-            "filename": file.filename,
+            "message": "Video converted to audio successfully",
+            "filename": f"{os.path.splitext(file.filename)[0]}.wav",
+            "filepath": audio_path
+        }
+
+    except Exception as e:
+        return JSONResponse(
+            status_code=500,
+            content={"message": f"Failed to process video: {str(e)}"}
+        )
+
+def transcribe_audio(audio_path):
+    transcription = model.transcribe(audio_path)
+    return transcription["text"].strip()
+
+@app.post("/audio-to-text", response_class=JSONResponse)
+async def convert_audio_to_text(audio_file: AudioFile):
+    try:
+
+        audio_path = os.path.join(UPLOAD_DIR, audio_file.filename)
+        transcript = transcribe_audio(audio_path)
+        os.remove(audio_path)
+
+        return {
+            "message": "Video transcribed successfully",
+            "filename": audio_file.filename,
             "transcript": transcript,
             "note": "Transcripts are subject to confidential information, generated data is 90% accurate."
         }
@@ -77,3 +81,4 @@ async def upload_video(file: UploadFile):
             status_code=500,
             content={"message": f"Failed to process video: {str(e)}"}
         )
+    
